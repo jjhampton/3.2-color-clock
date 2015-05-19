ff125bb66c74251f84542786f52b98d0d0ffc583
John Hampton <john_j_hampton@yahoo.com>
2015-05-19 15:22:44 -0400

    Set console.log to display the current time every second

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,35 @@
 (function() {
   "use strict";
 
-  alert("YO");
+  // Global Variables
+  var currentDate;
+  var hours;
+  var minutes;
+  var seconds;
+  var timeArray;
 
+
+
+  function getTime() {
+    currentDate = new Date(); // Assigns current date and time as a new Date object to variable
+    hours = currentDate.getHours();
+    minutes = currentDate.getMinutes();
+    seconds = currentDate.getSeconds();
+    timeArray = [hours, minutes, seconds];
+  }
+
+  //Function that returns time unit as a two-digit number if it is less than 10.  credit: http://stackoverflow.com/questions/8043026/javascript-format-number-to-have-2-digit users: nickolusroy, apfz
+  function setTimeFormat(unitOfTime, index, array) {
+    var twoDigitTime = unitOfTime >= 10 ? unitOfTime : "0"+ unitOfTime.toString();
+    array[index] = twoDigitTime;
+  }
+
+  function logTime() {
+    getTime();
+    timeArray.forEach(setTimeFormat);
+    console.log("timeArray map working");
+    console.log(timeArray[0] + ":" + timeArray[1] + ":" +  timeArray[2]);
+  }
+
+  setInterval(logTime, 1000);
 })();
