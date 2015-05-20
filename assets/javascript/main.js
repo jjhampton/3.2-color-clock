d366b7a9898b5eee899165ca06050669783fc364
John Hampton <john_j_hampton@yahoo.com>
2015-05-20 00:39:56 -0400

    Refactor Javascript to eliminate unneeded code and condense/rename functions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,84 +1,29 @@
 (function() {
   "use strict";
 
-  // Global Variables
-  var currentDate;
-  var hours;
-  var minutes;
-  var seconds;
-  var timeArray;
-
-  var $hours = document.querySelector(".clock .hours");
-  var $minutes = document.querySelector(".clock .minutes");
-  var $seconds = document.querySelector(".clock .seconds");
-  var $timerBar = document.querySelector(".timer-bar");
-
-
-
+  //Returns an array containing current time in hours, minutes, & seconds.
   function getTime() {
+    var currentDate;
+    var currentTime;
+    var hours;
+    var minutes;
+    var seconds;
+
     currentDate = new Date(); // Assigns current date and time as a new Date object to variable
-    hours = currentDate.getHours();
-    minutes = currentDate.getMinutes();
-    seconds = currentDate.getSeconds();
-    timeArray = [hours, minutes, seconds];
+    currentTime = [currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds()];
+    return currentTime;
   }
 
-  //Function that returns input value as a two-digit number if it is less than 10.  credit: http://stackoverflow.com/questions/8043026/javascript-format-number-to-have-2-digit users: nickolusroy, apfz
-  function setDigitFormatArray(unitOfTime, index, array) {
+  //Callback function to use w/ array.forEach().  Checks array's index values to see if they are less than 10, and if not, adds "0" to them, and re-inserts them into the array.  credit: http://stackoverflow.com/questions/8043026/javascript-format-number-to-have-2-digit users: nickolusroy, apfz
+  function formatDigitArray(unitOfTime, index, array) {
     var twoDigit = unitOfTime >= 10 ? unitOfTime : "0" + unitOfTime.toString();
     array[index] = twoDigit;
   }
 
-  function setDigitFormatString(string) {
+  //Setter function for String, similar to previous array function.  If string is not two-digits, prepends "0".  Returns two-digit string.
+  function formatDigitString(string) {
     var twoDigit = (string.length > 1) ? string : "0" + string;
     return twoDigit;
-  }
-
-  //logs percentage of a minute that the current seconds represents
-  function logPercentOfMinute(seconds) {
-    var percent;
-
-    seconds = Number(seconds);
-    percent = seconds / 60;
-    console.log(percent);
-  }
-
-  //returns hexadecimal value that the current unit of time represents
-  function getHexValue(unitOfTime) {
-    var hexValue;
-    hexValue = unitOfTime.toString(16);
-    return hexValue;
-  }
-
-  //returns a string of the complete hexadecimal color that the hex values for each unit of time represents
-  function getHexColor(hoursHex, minutesHex, secondsHex) {
-    var hexColor;
-
-    hexColor = hoursHex + minutesHex + secondsHex;
-    return hexColor.toString();
-  }
-
-  //Logs time and percent/minute to the console
-  function logTime() {
-    var hexHours;
-    var hexMinutes;
-    var hexSeconds;
-    var hexColor;
-
-    getTime();
-    timeArray.forEach(setDigitFormatArray);
-    console.log(timeArray[0] + ":" + timeArray[1] + ":" +  timeArray[2]);
-    logPercentOfMinute(timeArray[2]);
-    hexHours = getHexValue(timeArray[0]);
-    hexMinutes = getHexValue(timeArray[1]);
-    hexSeconds = getHexValue(timeArray[2]);
-    hexHours = setDigitFormatString(hexHours);
-    hexMinutes = setDigitFormatString(hexMinutes);
-    hexSeconds = setDigitFormatString(hexSeconds);
-    hexColor = getHexColor(hexHours, hexMinutes, hexSeconds);
-
-
-    console.log(hexColor);
   }
 
   //returns percentage of a minute that the current seconds represents
@@ -88,7 +33,38 @@
     return percent;
   }
 
-  //Function to "programmatically lighten or darken a hex color by a specific amount. Just pass in a string like "3F6D2A" for the color (col) and an base10 integer (amt) for the amount to lighten or darken. To darken, pass in a negative number (i.e. -20)."
+  //Returns a string of a two-digit hexadecimal byte value that a unit of time represents
+  function getHexByte(unitOfTime) {
+    var hexValue;
+    hexValue = unitOfTime.toString(16);
+    return hexValue;
+  }
+
+  //Returns a string of the hex triplet combined from three separate hex bytes
+  function getHexTriplet(hexByte1, hexByte2, hexByte3) {
+    var hexTriplet;
+
+    hexTriplet = (hexByte1 + hexByte2 + hexByte3).toString();
+    return hexTriplet;
+  }
+
+  //Logs time and percent/minute to the console
+  function logTime() {
+    var currentTime;
+    var hexColor;
+
+    currentTime = getTime();
+    currentTime.forEach(formatDigitArray);
+    console.log(currentTime[0] + ":" + currentTime[1] + ":" +  currentTime[2]);
+    console.log(getPercentOfMinute(currentTime[2]));
+
+    currentTime = currentTime.map(getHexByte);
+    currentTime = currentTime.map(formatDigitString);
+    hexColor = getHexTriplet(currentTime[0], currentTime[1], currentTime[2]);
+    console.log(hexColor);
+  }
+
+  //Function to "programmatically lighten or darken a hex color by a specific amount. Just pass in a string like "3F6D2A" for the color (col) and an base10 integer (amt) for the amount to lighten or darken. To darken, pass in a negative number (i.e. -20)." credit: http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
 
   function lightenDarkenColor(col,amt) {
     var num = parseInt(col,16);
@@ -99,40 +75,43 @@
     return newColor.toString(16);
   }
 
-//Displays time to spans within page's clock element and resizes <hr> corresponding to seconds w/ getPercentOfMinute()
-  function displayTime() {
+  //Displays time to spans within page's clock element.  Resizes <hr> corresponding to seconds w/ getPercentOfMinute().  Calls helper functions to generate hexadecimal color from screen time and set hex values for page's background gradient.
+  function displayToScreen() {
+    var currentTime;
     var timerBarSize; //Size to set $timerBar to
-    var hexHours;
-    var hexMinutes;
-    var hexSeconds;
-    var hexColorFrom;
     var hexColorTo;
+    var hexColorFrom;
 
-    getTime();
-    timeArray.forEach(setDigitFormatArray);
-    //console.log(timeArray[0] + ":" + timeArray[1] + ":" +  timeArray[2]);
-    $hours.textContent = timeArray[0];
-    $minutes.textContent = timeArray[1];
-    $seconds.textContent = timeArray[2];
+    //DOM elements
+    var $hours = document.querySelector(".clock .hours");
+    var $minutes = document.querySelector(".clock .minutes");
+    var $seconds = document.querySelector(".clock .seconds");
+    var $timerBar = document.querySelector(".timer-bar");
 
-    timerBarSize = getPercentOfMinute(timeArray[2]);
+    //Get current time and set units as properties for the corresponding HTML elements
+    currentTime = getTime();
+    currentTime.forEach(formatDigitArray);
+    $hours.textContent = currentTime[0];
+    $minutes.textContent = currentTime[1];
+    $seconds.textContent = currentTime[2];
 
+    //Get percent of current minute and set it as the width property of the timer-bar <hr>
+    timerBarSize = getPercentOfMinute(currentTime[2]);
     $timerBar.style.width = (timerBarSize * 100) + "%";
 
-    hexHours = getHexValue(timeArray[0]);
-    hexMinutes = getHexValue(timeArray[1]);
-    hexSeconds = getHexValue(timeArray[2]);
-    hexHours = setDigitFormatString(hexHours);
-    hexMinutes = setDigitFormatString(hexMinutes);
-    hexSeconds = setDigitFormatString(hexSeconds);
-    hexColorTo = getHexColor(hexHours, hexMinutes, hexSeconds);
+    //Generate hex values from current time
+    currentTime = currentTime.map(getHexByte);
+    currentTime = currentTime.map(formatDigitString);
+
+    // Determine hex color to use for the to and from gradients
+    hexColorTo = getHexTriplet(currentTime[0], currentTime[1], currentTime[2]);
     hexColorFrom = lightenDarkenColor(hexColorTo, 50);
 
-
+    //Assign hex colors to gradients
     document.body.style.backgroundImage = "-webkit-gradient(radial, 50% 50%, 200, 50% 50%, 700, from(#" + hexColorFrom + "), to(#" + hexColorTo + "))";
   }
 
   setInterval(logTime, 1000);
-  setInterval(displayTime, 100);
+  setInterval(displayToScreen, 1000);
 
 })();
